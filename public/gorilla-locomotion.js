394bc4b7717b8ff9b31abacdd9f3873d6c2e7c85
2ndsebastiantablet-hash <2ndsebastiantablet@gmail.com>
2026-04-25 20:51:27 -0400

    Add VR menu, turning, and object pickup

--- public/gorilla-locomotion.js
+++ public/gorilla-locomotion.js
@@ -1,38 +1,50 @@
 (function () {
-  // The live game now uses simple Quest locomotion instead of Gorilla Tag hand pushing.
-  // Left joystick moves the rig and the right controller jump button applies a fixed jump.
+  AFRAME.registerComponent("locomotion-collider", {
+    schema: {
+      type: { default: "box" },
+      size: { type: "vec3", default: { x: 1, y: 1, z: 1 } }
+    }
+  });
+
   AFRAME.registerComponent("simple-vr-locomotion", {
     schema: {
       leftController: { type: "selector" },
       rightController: { type: "selector" },
       camera: { type: "selector" },
       moveSpeed: { default: 3.25 },
+      turnSpeed: { default: 120 },
       jumpVelocity: { default: 4.75 },
       gravity: { default: -11 },
       floorY: { default: 0 }
     },
 
     init: function () {
-      this.input = { x: 0, y: 0 };
+      this.moveInput = { x: 0, y: 0 };
+      this.turnInput = 0;
       this.velocityY = 0;
       this.grounded = true;
+      this.worldQuaternion = new THREE.Quaternion();
       this.yawEuler = new THREE.Euler(0, 0, 0, "YXZ");
       this.forward = new THREE.Vector3();
       this.right = new THREE.Vector3();
       this.move = new THREE.Vector3();
 
-      this.onThumbstickMoved = this.onThumbstickMoved.bind(this);
-      this.onThumbstickEnd = this.onThumbstickEnd.bind(this);
+      this.onLeftThumbstickMoved = this.onLeftThumbstickMoved.bind(this);
+      this.onLeftThumbstickEnd = this.onLeftThumbstickEnd.bind(this);
+      this.onRightThumbstickMoved = this.onRightThumbstickMoved.bind(this);
+      this.onRightThumbstickEnd = this.onRightThumbstickEnd.bind(this);
       this.onJumpButton = this.onJumpButton.bind(this);
     },
 
     play: function () {
       if (this.data.leftController) {
-        this.data.leftController.addEventListener("thumbstickmoved", this.onThumbstickMoved);
-        this.data.leftController.addEventListener("thumbsticktouchend", this.onThumbstickEnd);
+        this.data.leftController.addEventListener("thumbstickmoved", this.onLeftThumbstickMoved);
+        this.data.leftController.addEventListener("thumbsticktouchend", this.onLeftThumbstickEnd);
       }
 
       if (this.data.rightController) {
+        this.data.rightController.addEventListener("thumbstickmoved", this.onRightThumbstickMoved);
+        this.data.rightController.addEventListener("thumbsticktouchend", this.onRightThumbstickEnd);
         this.data.rightController.addEventListener("abuttondown", this.onJumpButton);
         this.data.rightController.addEventListener("bbuttondown", this.onJumpButton);
       }
@@ -40,24 +52,34 @@
 
     pause: function () {
       if (this.data.leftController) {
-        this.data.leftController.removeEventListener("thumbstickmoved", this.onThumbstickMoved);
-        this.data.leftController.removeEventListener("thumbsticktouchend", this.onThumbstickEnd);
+        this.data.leftController.removeEventListener("thumbstickmoved", this.onLeftThumbstickMoved);
+        this.data.leftController.removeEventListener("thumbsticktouchend", this.onLeftThumbstickEnd);
       }
 
       if (this.data.rightController) {
+        this.data.rightController.removeEventListener("thumbstickmoved", this.onRightThumbstickMoved);
+        this.data.rightController.removeEventListener("thumbsticktouchend", this.onRightThumbstickEnd);
         this.data.rightController.removeEventListener("abuttondown", this.onJumpButton);
         this.data.rightController.removeEventListener("bbuttondown", this.onJumpButton);
       }
     },
 
-    onThumbstickMoved: function (event) {
-      this.input.x = event.detail.x || 0;
-      this.input.y = event.detail.y || 0;
+    onLeftThumbstickMoved: function (event) {
+      this.moveInput.x = event.detail.x || 0;
+      this.moveInput.y = event.detail.y || 0;
     },
 
-    onThumbstickEnd: function () {
-      this.input.x = 0;
-      this.input.y = 0;
+    onLeftThumbstickEnd: function () {
+      this.moveInput.x = 0;
+      this.moveInput.y = 0;
+    },
+
+    onRightThumbstickMoved: function (event) {
+      this.turnInput = event.detail.x || 0;
+    },
+
+    onRightThumbstickEnd: function () {
+      this.turnInput = 0;
     },
 
     onJumpButton: function () {
@@ -80,7 +102,12 @@
         return;
       }
 
-      this.yawEuler.setFromQuaternion(this.data.camera.object3D.quaternion);
+      // Smooth turn using the right joystick X axis.
+      this.el.object3D.rotation.y -= THREE.MathUtils.degToRad(this.turnInput * this.data.turnSpeed * deltaTime);
+
+      // Move in the direction the headset is facing, but ignore headset pitch and roll.
+      this.data.camera.object3D.getWorldQuaternion(this.worldQuaternion);
+      this.yawEuler.setFromQuaternion(this.worldQuaternion);
       this.yawEuler.x = 0;
       this.yawEuler.z = 0;
 
@@ -88,8 +115,8 @@
       this.right.set(1, 0, 0).applyEuler(this.yawEuler);
 
       this.move.set(0, 0, 0);
-      this.move.addScaledVector(this.right, this.input.x);
-      this.move.addScaledVector(this.forward, -this.input.y);
+      this.move.addScaledVector(this.right, this.moveInput.x);
+      this.move.addScaledVector(this.forward, -this.moveInput.y);
 
       if (this.move.lengthSq() > 1) {
         this.move.normalize();
